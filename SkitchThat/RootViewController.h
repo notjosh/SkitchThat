45c0bcb938720915ed2f802df4940a1f8875d3eb
Joshua May <joshua.may@db1.org>
2011-06-21 13:12:17 +0200

    Initial Commit

---
+++ SkitchThat/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SkitchThat
+//
+//  Created by compo on 6/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ SkitchThat/SkitchThatAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SkitchThatAppDelegate.h
+//  SkitchThat
+//
+//  Created by compo on 6/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkitchThatAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
