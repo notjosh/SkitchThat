cad403e347ea7ae703c88ba6a22b39fa622c0cfb
Joshua May <notjosh@gmail.com>
2011-06-26 17:25:21 +0200

    persisting user preferences on app hide/close

--- SkitchThat/SkitchThatAppDelegate.m
+++ SkitchThat/SkitchThatAppDelegate.m
@@ -46,6 +46,8 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
+
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -66,6 +68,8 @@
      Save data if appropriate.
      See also applicationDidEnterBackground:.
      */
+
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)setState:(kSkitchThatAppDelegateState)state {
